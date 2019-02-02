a1f515b96d9586e47c0dd9e3836a1fc0fd461a98
Vanilla23 <47268355+Vanilla23@users.noreply.github.com>
2019-02-02 02:55:23 -1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          /*  var N = int.Parse(Console.ReadLine());
+            var odd = 0.0;
+            var even = 0.0;
+            for(var i=1; i<=N; i++)
+            {
+                var element = int.Parse(Console.ReadLine());
+                if (i % 2 == 0) odd += element;
+                else even += element;
+            }
+            if (odd == even) Console.WriteLine("Yes, sum= " + odd);
+            else Console.WriteLine("No, diff= " + Math.Abs(odd - even));
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
